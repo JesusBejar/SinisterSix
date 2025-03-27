a8b3db2a09a7b7c4b510fab6e5fe3013e6377f78
Jesus Bejar <israelbejarii@gmail.com>
2025-03-27 17:54:21 -0600

    got one user

--- Vulture/scripts.js
+++ Vulture/scripts.js
@@ -4,8 +4,11 @@ fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   // handle data
   .then((data) => {
-    // log data (then inject into html)
     console.log(data);
+    // log data (then inject into html)
+    const { name: name, email: email, address: address } = data[0];
+    // returns one user , the first one
+    console.log(name, email, address);
   })
   // error handling
   .catch((error) => {
