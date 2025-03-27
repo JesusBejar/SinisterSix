449f24921351ae6a723b5864a9d23a12e673d1d9
Jesus Bejar <israelbejarii@gmail.com>
2025-03-27 17:27:31 -0600

    added comments

---
+++ Vulture/scripts.js
@@ -0,0 +1,11 @@
+// fetch + url (returns postive or negative Promise)
+
+// parse response as JSON using .then
+
+// handle data
+
+// log data (then inject into html)
+
+// error handling
+
+// what does the .then keyword refer to???
