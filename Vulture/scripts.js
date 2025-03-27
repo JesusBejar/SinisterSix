6cf391f6555c72d0eaee5eeabb86628d23107169
Jesus Bejar <israelbejarii@gmail.com>
2025-03-27 17:33:07 -0600

    logged user data

--- Vulture/scripts.js
+++ Vulture/scripts.js
@@ -1,11 +1,13 @@
 // fetch + url (returns postive or negative Promise)
-
-// parse response as JSON using .then
-
-// handle data
-
-// log data (then inject into html)
-
-// error handling
-
-// what does the .then keyword refer to???
+fetch("https://jsonplaceholder.typicode.com/users")
+  // parse response as JSON using .then
+  .then((response) => response.json())
+  // handle data
+  .then((data) => {
+    // log data (then inject into html)
+    console.log(data);
+  })
+  // error handling
+  .catch((error) => {
+    console.error("Error: ", error);
+  });
