4618f93050b8d45cc54a4705ddc99231b6c65bfd
Jesus Bejar <israelbejarii@gmail.com>
2025-03-28 09:21:46 -0600

    injected user list into html

--- Vulture/scripts.js
+++ Vulture/scripts.js
@@ -1,16 +1,19 @@
+const list = document.querySelector("#display");
 // fetch + url (returns postive or negative Promise)
 fetch("https://jsonplaceholder.typicode.com/users")
   // parse response as JSON using .then
   .then((response) => response.json())
   // handle data
   .then((data) => {
+    let content = "";
     data.forEach((user) => {
-      //   console.log(data);
       // log data (then inject into html)
+      // console.log(data);
       const { name, email, address } = user;
       // returns all users
-      console.log(name, email, address);
+      content += `<li> ${name}, ${email}, ${address.city} </li>`;
     });
+    list.innerHTML = content;
   })
   // error handling
   .catch((error) => {
