fad437c71e73ccec464a907e31e4cc81731d8640
Jesus Bejar <israelbejarii@gmail.com>
2025-03-28 17:56:07 -0600

    installed axios, added comments

---
+++ Mysterio/src/api/fetchData.js
@@ -0,0 +1,14 @@
+// import axios
+import axios from "axios";
+
+// make and export arrow function w/ axios
+
+// return content
+
+// send request
+
+// get response from url
+
+// error handling
+
+// call function within App.jsx
