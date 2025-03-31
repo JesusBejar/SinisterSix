929849ad612851ecd62ae5f83e3a9a8afe24758b
Jesus Bejar <israelbejarii@gmail.com>
2025-03-31 09:42:37 -0600

    added useState and useEffect

--- Mysterio/src/App.jsx
+++ Mysterio/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
-import { FetchData } from "./api/fetchData";
+import { useEffect, useState } from "react";
+import { fetchData } from "./api/fetchData";
 
 function App() {
   // useState to store user data
+  const [users, setUsers] = useState([]);
   // useEffect to fetch user data upon mount (calls FetchData)
+  useEffect(() => {
+    fetchData()
+      .then((data) => setUsers(data))
+      // error handling
+      .catch((error) => console.error("Error: ", error));
+    // empty dependancy means run once (upon mount)
+  }, []);
 
   // return user data using jsx
-  return "no";
+  console.log("users", users);
+  return "";
 }
 
 export default App;
